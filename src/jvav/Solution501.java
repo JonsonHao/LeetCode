956851fbb29940a52e52ee9cb6f151a0d32465d3
DESKTOP-ANIM8AP\linjunhao <linjunhao4257@gmail.com>
2020-09-24 10:37:32 +0800

    add Solution501

---
+++ src/jvav/Solution501.java
@@ -0,0 +1,66 @@
+package jvav;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author created by linjunhao
+ * @date 2020/9/24
+ * @description LeetCode 算法题第 501 题：把二叉树转换成累加树
+ * @tag：二叉树
+ * @difficulty：easy
+ */
+public class Solution501 {
+    int base, count, maxCount;
+    List<Integer> answer = new ArrayList<>();
+
+    /**
+     * Morris 中序遍历
+     * @param root
+     * @return
+     */
+    public int[] findMode(TreeNode root) {
+        TreeNode cur = root, pre = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                update(cur.val);
+                cur = cur.right;
+                continue;
+            }
+            pre = cur.left;
+            while (pre.right != null && pre.right != cur) {
+                pre = pre.right;
+            }
+            if (pre.right == null) {
+                pre.right = cur;
+                cur = cur.left;
+            } else {
+                pre.right = null;
+                update(cur.val);
+                cur = cur.right;
+            }
+        }
+        int[] mode = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            mode[i] = answer.get(i);
+        }
+        return mode;
+    }
+
+    private void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.add(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer.clear();
+            answer.add(base);
+        }
+    }
+}
